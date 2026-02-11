c53a9f035ea99bb833471633b93d267a721a1518
Roberto Villegas Velázquez <villegasvelazquez@hotmail.com>
2026-02-11 12:22:19 -0600

    Semana 2, día 3: refactor usando variable booleana para controlar estado

--- semana2/dia3_menu.py
+++ semana2/dia3_menu.py
@@ -1,35 +1,22 @@
-opcion = "1"
+opcion = ""
+secreto_mostrado = False
 
 while opcion != "salir":
     print("\nMenú:")
     print("1 - Saludar")
     print("2 - Mostrar número secreto")
-    print("\nEscribe 'Salir' para terminar")
+    print("\nEscribe 'salir' para terminar")
     
     opcion = input("\nElige una opción: ")
     
     if opcion == "1":
         print("\nHola, humano.")
     elif opcion == "2":
-        print("\nEl número secreto es 42.")
-        while opcion == "2":
-            print("\nMenú:")
-            print("1 - Saludar")
-            print("2 - Mostrar número secreto")
-            print("\nEscribe 'Salir' para terminar")
-            
-            opcion = input("\nElige una opción: ")
-            
-            if opcion == "1":
-                print("\nHola, humano.")
-                opcion = "2"
-            elif opcion == "2":
-                print("\nYa no es posible consultar el número secreto.")
-            elif opcion == "salir":
-                print("\nCerrando programa")
-                opcion = "salir"
+            if not secreto_mostrado:
+                    print("\nEl número secreto es 42.")
+                    secreto_mostrado = True
             else:
-                print("\nOpción inválida")
+                print("\nYa no es posible consultar el número secreto.")
     elif opcion == "salir":
             print("\nCerrando programa...")
     else:
