5d0304f4c4ee5571aa62213090ce5e36bcc75324
Roberto Villegas Velázquez <villegasvelazquez@hotmail.com>
2026-02-12 10:39:35 -0600

    Semana 2, día 3: while, for, try y except, para valores inválidos en input

--- semana2/dia3_for_input.py
+++ semana2/dia3_for_input.py
@@ -1,8 +1,14 @@
-limite = int(input("¿Hasta qué número quieres contar? (sólo números del 0 en adelante): "))
+while True:
+    try:
+        limite = int(input("¿Hasta qué número quieres contar? (sólo números del 0 en adelante): "))
 
-while limite < 0:
-    print("Ingresa unicamente cero o números enteros positivos")
-    limite = int(input("¿Hasta qué número quieres contar? (sólo números del 0 en adelante): "))
+        if limite < 0:
+            print("Ingresa unicamente cero o números enteros positivos")
+        else:
+            break
+            
+    except ValueError:
+        print("Eso no es un número válido. Intenta de nuevo.")
 
 for numero in range(1, limite + 1):
         print("Número:", numero)
